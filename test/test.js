7753d06cd3b80ac04f7e34153022e9788b0e8612
damartripamungkas <damartripamungkas@github.com>
2023-09-26 00:11:03 +0700

    test: update code for new version

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
-const EthRpc = require('../index');
-const provider = new EthRpc("https://bscrpc.com", (err) => {
-    console.log(`[ERR] ${err}`);
-});
+const { Provider, EthNameSpace } = require("../index");
+const provider = new Provider("https://bscrpc.com");
+const ethName = new EthNameSpace();
 
 (async () => {
     /**
@@ -10,46 +9,20 @@ const provider = new EthRpc("https://bscrpc.com", (err) => {
      */
     await provider.isReady();
 
-    /**
-     * test method .send()
-     * @param string method - method eth name space 
-     * @param array params - parameter request
-     * @returns hex
-     */
-    const getChainId = await provider.send("eth_chainId", []);
-    console.log("🚀 ~ file: test.js:18 ~ getChainId:", BigInt(getChainId.result));
+    // test method .send()
+    const getChainId = await provider.send(ethName.eth_chainId());
+    console.log("🚀 ~ file: test.js:20 ~ getChainId:", getChainId);
 
-    /**
-     * test method .sendBatch()
-     * @param arrayObject [ { method:"", params:[] } ]
-     * @returns array
-     */
-    const getBlockNumberAndChainId = await provider.sendBatch([
-        { method: "eth_blockNumber", params: [] },
-        { method: "eth_chainId", params: [] }
-    ]);
-    console.log("🚀 ~ file: test.js:29 ~ getBlockNumberAndChainId", getBlockNumberAndChainId.map(it => BigInt(it.result)));
+    // test method .sendBatch()
+    const getBlockNumberAndChainId = await provider.sendBatch(
+        ethName.eth_blockNumber(),
+        ethName.eth_chainId()
+    );
+    console.log("🚀 ~ file: test.js:27 ~ getBlockNumberAndChainId:", getBlockNumberAndChainId);
 
-    // // unhidden this comment if you use network type ws or ipc
-    // /**
-    //  * test method .subscribe()
-    //  * @param string params - subscription name
-    //  * @param functionCallback callbackRes - callbackRes 1 param
-    //  * @returns undefined
-    //  */
-    // let subId = 0;
-    // provider.subscribe("newPendingTransactions", (res) => {
-    //     if (res.id !== undefined) {
-    //         subId = res.result;
-    //     } else {
-    //         console.log("🚀 ~ file: test.js:42 ~ provider.subscribe ~ res:", res.result)
-    //     }
+    // // remove this comment for run method
+    // test method .subscribe()
+    // provider.subscribe(ethName.eth_subscribe("newPendingTransactions"), (result) => {
+    //     console.log("🚀 ~ file: test.js:31:", result);
     // });
-
-    // /**
-    //  * test method .unSubscribe()
-    //  * @param string subId - subscription id
-    //  * @returns bool
-    //  */
-    // const unSubscribe = await provider.unSubscribe(subId);
 })();
