bfcd5634f794fdd8f3d18b2aaef2ab2a723b768d
damartripamungkas <damartripamungkas@github.com>
2023-09-28 19:43:20 +0700

    test: remake code

--- test/test.js
+++ test/test.js
@@ -11,18 +11,12 @@ const ethName = new EthNameSpace();
 
     // test function .send()
     const getChainId = await provider.send(ethName.eth_chainId());
-    console.log("🚀 ~ file: test.js:20 ~ getChainId:", getChainId);
+    console.log(`INFO: result getChainId:`, getChainId);
 
     // test function .sendBatch()
     const getBlockNumberAndChainId = await provider.sendBatch(
         ethName.eth_blockNumber(),
         ethName.eth_chainId()
     );
-    console.log("🚀 ~ file: test.js:27 ~ getBlockNumberAndChainId:", getBlockNumberAndChainId);
-
-    // test function .subscribe() and method unsubscribe
-    // await provider.subscribe(ethName.eth_subscribe("newPendingTransactions"), (result, subsId) => {
-    //     console.log("🚀 ~ file: test.js:31:", result);
-    //     await provider.send(ethName.eth_unsubscribe(subsId));
-    // });
+    console.log(`INFO: result getBlockNumberAndChainId:`, getBlockNumberAndChainId);
 })();
