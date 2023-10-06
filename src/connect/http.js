f19cef97f0b2d3559de1b132fc70260c2fcfe1ba
damartripamungkas <damartripamungkas@github.com>
2023-10-07 01:38:49 +0700

    fix: add error triggers to prevent networks that are not supported

--- src/connect/http.js
+++ src/connect/http.js
@@ -1,27 +1,28 @@
 const { default: fetch } = require("node-fetch-commonjs");
 
 class Main {
-    constructor(url = "", socketOpt = {}) {
-        this.url = url;
-        this.client = {};
-        this.client.request = async (data) => {
-            const init = Object.assign(
-                {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(data)
-                },
-                socketOpt
-            );
-            const res = await fetch(this.url, init);
-            const jsonData = await res.json();
-            return jsonData;
-        }
-    }
+	constructor(url = "", socketOpt = {}) {
+		this.url = url;
+		this.client = {};
+		this.client.on = () => {};
+		this.client.request = async (data) => {
+			const init = Object.assign(
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(data),
+				},
+				socketOpt
+			);
+			const res = await fetch(this.url, init);
+			const jsonData = await res.json();
+			return jsonData;
+		};
+	}
 
-    isReady = () => {
-        return true;
-    }
+	isReady = () => {
+		return true;
+	};
 }
 
 module.exports = Main;
