062d8eda33b4bfb9e8424750f1b63c8f845598a8
damartripamungkas <damartripamungkas@github.com>
2023-10-25 12:11:01 +0700

    test: update test

--- test/test.js
+++ test/test.js
@@ -1,22 +1,18 @@
-const { Provider, EthNameSpace } = require("../index");
+const { Provider, EthNameSpace } = require("..");
 const provider = new Provider("https://bscrpc.com");
 const ethName = new EthNameSpace();
 
 (async () => {
-    /**
-     * first run function isReady before send request
-     * @returns bool
-     */
-    await provider.isReady();
+  /**
+   * first run function isReady before send request
+   * @returns bool
+   */
+  await provider.isReady();
 
-    // test function .send()
-    const getChainId = await provider.send(ethName.eth_chainId());
-    console.log(`INFO: result getChainId:`, getChainId);
+  const getChainId = await provider.send(ethName.eth_chainId());
+  console.log(`INFO: result getChainId:`, getChainId);
 
-    // test function .sendBatch()
-    const getBlockNumberAndChainId = await provider.sendBatch(
-        ethName.eth_blockNumber(),
-        ethName.eth_chainId()
-    );
-    console.log(`INFO: result getBlockNumberAndChainId:`, getBlockNumberAndChainId);
+  // test function .sendBatch()
+  const getBlockNumberAndChainId = await provider.sendBatch(ethName.eth_blockNumber(), ethName.eth_chainId());
+  console.log(`INFO: result getBlockNumberAndChainId:`, getBlockNumberAndChainId);
 })();
--- test/testSubscribe.js
+++ test/testSubscribe.js
@@ -1,28 +1,28 @@
-const { Provider, EthNameSpace } = require("../index");
+const { Provider, EthNameSpace } = require("..");
 const ethName = new EthNameSpace();
 const provider = new Provider(
-    "ws://localhost:1234",
-    {},
-    {
-        delay: 500,
-        autoReconnect: true,
-        maxAttempts: Number.MAX_SAFE_INTEGER
-    }
+  "ws://localhost:1234",
+  {},
+  {
+    delay: 500,
+    autoReconnect: true,
+    maxAttempts: Number.MAX_SAFE_INTEGER,
+  }
 );
 
 (async () => {
-    /**
-     * first run function isReady before send request
-     * @returns bool
-     */
-    await provider.isReady();
+  /**
+   * first run function isReady before send request
+   * @returns bool
+   */
+  await provider.isReady();
 
-    // test function .subscribe() and method unsubscribe
-    const buildSubscribe = ethName.eth_subscribe("newPendingTransactions", true);
-    await provider.subscribe(buildSubscribe, true, async (result, subsId) => {
-        console.log(`INFO: result stream data eth_subscribe:`, result);
+  // test function .subscribe() and method unsubscribe
+  const buildSubscribe = ethName.eth_subscribe("newPendingTransactions", true);
+  await provider.subscribe(buildSubscribe, true, async (result, subsId) => {
+    console.log(`INFO: result stream data eth_subscribe:`, result);
 
-        const unsubs = await provider.send(ethName.eth_unsubscribe(subsId));
-        console.log(`INFO: unsubscribe success with subsId: ${unsubs}`);
-    });
+    const unsubs = await provider.send(ethName.eth_unsubscribe(subsId));
+    console.log(`INFO: unsubscribe success with subsId: ${unsubs}`);
+  });
 })();
